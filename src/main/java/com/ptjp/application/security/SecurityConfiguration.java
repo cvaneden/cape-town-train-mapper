7a7c05da19cc8415a1a506b48f9a793a3dbdbf25
cvaneden <100783814+cvaneden@users.noreply.github.com>
2022-09-26 21:40:49 +0200

    Update SecurityConfiguration.java

--- src/main/java/com/ptjp/application/security/SecurityConfiguration.java
+++ src/main/java/com/ptjp/application/security/SecurityConfiguration.java
@@ -15,7 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
     public static final String LOGOUT_URL = "/";
-
+    
+    public SecurityConfiguration() {}
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
