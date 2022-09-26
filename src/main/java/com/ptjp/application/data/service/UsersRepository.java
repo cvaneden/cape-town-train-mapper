65c3c284c92bf1517c93d5ff1973084086eaaa39
cvaneden <100783814+cvaneden@users.noreply.github.com>
2022-09-26 21:36:46 +0200

    Delete UsersRepository.java

--- src/main/java/com/ptjp/application/data/service/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ptjp.application.data.service;
-
-import com.ptjp.application.data.entity.Users;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, UUID> {
-
-}
