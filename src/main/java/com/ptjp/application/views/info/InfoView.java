852b6a0c546e89432f1c0cf6b494aefd83292d9b
cvaneden <100783814+cvaneden@users.noreply.github.com>
2022-09-26 22:02:11 +0200

    Update InfoView.java

--- src/main/java/com/ptjp/application/views/info/InfoView.java
+++ src/main/java/com/ptjp/application/views/info/InfoView.java
@@ -2,8 +2,7 @@ package com.ptjp.application.views.info;
 
 import com.ptjp.application.views.MainLayout;
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -18,13 +17,21 @@ public class InfoView extends VerticalLayout {
         // Information about our project and what we want to achieve as a team
         // Developers, information and goals of project
 
-        add(new H1("Welcome to our journey planner web application to navigate the quickest way " +
-                "for you to get to your desired destination"));
+        add(new H1("Cape Town Train Mapper"));
 
-        add(new H2("This is a project constructed by UCT 3rd year students with the design and architecture being constructed by Conor Van Eden" +
-                " and developers Michael Wade and Zac Schmidt"));
+        add(new Text("This is a Capstone project built by third year UCT students who are currently enrolled in CSC3003S, which is a second semester course in Computer Science at the University of Cape Town."));
 
+        add(new H3("What is our aim?"));
+        add(new Text("The aim of this application is to navigate through the city of Cape Town in an easy and efficient way using trains. There are currently not many " +
+                "applications that provide an easy interface to interact with. Our application wishes to solve the problem of looking through complicated timetables " +
+                "to get to your desired destination."));
 
+        add(new H3("Who are we?"));
 
+        //Conor
+        Image img = new Image("images/me.png", "image of Conor");
+        System.out.println("Height = "+img.getHeight() +"; Width = " +img.getWidth());
+        add(img);
     }
+
 }
