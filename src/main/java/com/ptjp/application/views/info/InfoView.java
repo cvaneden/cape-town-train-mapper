6629f16d952721e8e717b47d80783bd2353a420f
cvaneden <100783814+cvaneden@users.noreply.github.com>
2022-09-27 14:18:08 +0200

    Update InfoView.java

--- src/main/java/com/ptjp/application/views/info/InfoView.java
+++ src/main/java/com/ptjp/application/views/info/InfoView.java
@@ -29,9 +29,26 @@ public class InfoView extends VerticalLayout {
         add(new H3("Who are we?"));
 
         //Conor
-        Image img = new Image("images/me.png", "image of Conor");
-        System.out.println("Height = "+img.getHeight() +"; Width = " +img.getWidth());
+        add(new H4("Conor Van Eden"));
+        Image img = new Image("images/Conor.png", "image of Conor");
         add(img);
+        add(new Text("Conor designed the interface and the overall structure of the code. He was responsible" +
+                " for all front-end functionality. He also led to security side of the application " +
+                "and implemented all the login and sign-up features."));
+
+        //Zac
+        add(new H4("Zac Schmidt"));
+        Image img1 = new Image("images/Zac.png", "image of Zac");
+        add(img1);
+        add(new Text("Zac designed the data structures and ensured the formatting of all files were correct. He was also responsible" +
+                " for testing and documenting progress along the way. He was the team leader. "));
+
+        //Michael
+        add(new H4("Michael Wade"));
+        Image img2 = new Image("images/Michael.png", "image of Michael");
+        add(img2);
+        add(new Text("Michael was our main developer and designed the route algorithm. He was responsible for all back-end" +
+                " functionality and implemented all the data structures."));
     }
 
 }
